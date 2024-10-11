0e8b3c770be6900ad38930ef641c1e5251d3c3dd
sg-acloset <sg.acloset@gmail.com>
2024-10-11 18:11:27 +0900

    service group 필드 추가

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 go build -o http_proxy ./cmd/proxy/main.go
 cp http_proxy /home/http_proxy
+chown ec2-user:ec2-user /home/http_proxy
 cp http_proxy.service /etc/systemd/system/http_proxy.service
 systemctl restart http_proxy.service
